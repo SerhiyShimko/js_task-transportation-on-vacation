859d73e788541269e4d236ca3da05538779c99c9
Serhii Shimko <shimkosergij2010@gmail.com>
2026-01-13 19:14:00 +0200

    created a function that returns the total price

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,17 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const totalPrice = days * 40;
+
+  if (days >= 7) {
+    return totalPrice - 50;
+  }
+
+  if (days >= 3) {
+    return totalPrice - 20;
+  }
+
+  return totalPrice;
 }
 
 module.exports = calculateRentalCost;
